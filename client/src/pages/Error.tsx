57782604277b1b080d24620e7eab3f5bd2c16773
Farnood Saghi <farnood@Farnoods-MacBook-Air.local>
2022-09-02 01:56:36 +0800

    feat: add some skeleton files for pages and actions

---
+++ client/src/pages/Error.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+interface ErrorProps {}
+
+const Error: React.FC<ErrorProps> = ({}) => {
+  return <></>;
+};
+
+export default Error;
---
+++ client/src/pages/index.ts
@@ -0,0 +1,6 @@
+import Home from "./Home";
+import Error from "./Error";
+import Login from "./Login";
+import Signup from "./Signup";
+
+export { Home, Error, Login, Signup };
