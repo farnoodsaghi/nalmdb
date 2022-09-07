d62dcf38ca4b9511d4bcbfada0a97f5f3d072fb0
Farnood Saghi <farnood@Farnoods-MacBook-Air.local>
2022-09-07 23:13:55 +0800

    feat: finish setting up the topbar styling

--- client/src/components/TopBar.tsx
+++ client/src/components/TopBar.tsx
@@ -5,17 +5,17 @@ interface TopBarProps {}
 
 const TopBar: React.FC<TopBarProps> = ({}) => {
   return (
-    <nav className="flex flex-row justify-between items-baseline w-19/30f h-24 font-sarabun bg-carbon-black text-light-grey ml-1/6f mr-1/5f fixed top-0">
+    <nav className="flex flex-row justify-between items-baseline w-19/30f h-24 font-sarabun bg-carbon-black text-light-grey ml-1/6f mr-1/5f fixed top-0 z-50">
       <div className="flex flex-row justify-between gap-8 text-xl font-light items-start h-full mt-8 ml-8">
-        <h2>Movies</h2>
-        <h2>TV Shows</h2>
+        <h2 className="hover:text-white cursor-pointer">Movies</h2>
+        <h2 className="hover:text-white cursor-pointer">TV Shows</h2>
       </div>
       <div className="flex flex-row justify-between items-center bg-off-black text-light-grey rounded-md mr-8">
         <form>
           <input
             type="text"
             placeholder="Search"
-            className="bg-transparent p-3 border-none outline-none placeholder:text-lg"
+            className="bg-transparent p-3 border-none outline-none placeholder:text-lg placeholder:text-light-grey"
           />
         </form>
         <Icon icon="eva:search-fill" className="w-6 h-6 self-center mr-3" />
