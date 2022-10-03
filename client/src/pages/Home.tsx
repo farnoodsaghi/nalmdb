0b4ee63ff7964287bd9dc918ae674bf047d4c3cc
Farnood Saghi <farnood@Farnoods-MacBook-Air.local>
2022-10-04 05:21:33 +0800

    feat: add more sliders to home page

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,17 +1,27 @@
 import React from "react";
 import { SlideShow, MovieSliderBox } from "../components";
+import { MoviesContext } from "../context/moviesContext";
 
 interface HomeProps {}
 
 const Home: React.FC<HomeProps> = ({}) => {
+  const { active_topbar } = React.useContext(MoviesContext)!;
   return (
     <main className="block bg-carbon-black ml-1/6f h-full w-5/6 mt-24 mb-6">
       <SlideShow />
-      <MovieSliderBox title="Trending" type="trending" />
+      <MovieSliderBox title="Trending Now" type="trending" />
       <MovieSliderBox title="Top Rated" type="top_rated" />
-      <MovieSliderBox title="Action" type="action" />
+      <MovieSliderBox
+        title={active_topbar === 1 ? "Action" : "Action and Adventure"}
+        type="action"
+      />
       <MovieSliderBox title="Comedy" type="comedy" />
+      <MovieSliderBox title="Crime" type="crime" />
       <MovieSliderBox title="Drama" type="drama" />
+      <MovieSliderBox
+        title={active_topbar === 1 ? "Science Fiction" : "Fantasy and Sci-Fi"}
+        type="scifi"
+      />
     </main>
   );
 };
