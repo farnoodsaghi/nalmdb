f03f0ae2ffcad9a481605d9df70e54c316fd9411
Farnood Saghi <farnood@Farnoods-MacBook-Air.local>
2022-10-04 05:12:57 +0800

    feat: wrap component in react-router-dom link

--- client/src/components/MovieCard.tsx
+++ client/src/components/MovieCard.tsx
@@ -1,19 +1,22 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 interface MovieCardProps {
   children?: React.ReactNode;
   poster_path: string;
+  id: string;
 }
 
-const MovieCard: React.FC<MovieCardProps> = ({ children, poster_path }) => {
+const MovieCard: React.FC<MovieCardProps> = ({ poster_path, id }) => {
   return (
     <div className="flex justify-center items-center bg-dark-grey rounded-md cursor-pointer hover:brightness-75 snap-start max-h-full aspect-[1/1.5]">
-      {/* {children} */}
-      <img
-        src={`https://image.tmdb.org/t/p/original/${poster_path}`}
-        alt=""
-        className="aspect-[1/1.5] object-cover rounded-md"
-      />
+      <Link to={`/title/${id}`}>
+        <img
+          src={`https://image.tmdb.org/t/p/original/${poster_path}`}
+          alt=""
+          className="aspect-[1/1.5] object-cover rounded-md"
+        />
+      </Link>
     </div>
   );
 };
