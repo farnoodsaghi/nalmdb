8b6884452b23eb58b17a55482fa48d9f09bf9f1d
Farnood Saghi <farnood@Farnoods-MacBook-Air.local>
2022-10-07 05:07:35 +0800

    feat: create a new component for cast list items

--- client/src/components/CastListItem.tsx
+++ client/src/components/CastListItem.tsx
@@ -1,17 +1,36 @@
 import React from "react";
+import defaultProfile from "../assets/default_profile_path.png";
 
-interface CastListItemProps {}
+interface CastListItemProps {
+  name: string;
+  character: string;
+  profile_path: string;
+}
 
-const CastListItem: React.FC<CastListItemProps> = ({}) => {
+const CastListItem: React.FC<CastListItemProps> = ({
+  name,
+  character,
+  profile_path,
+}) => {
   return (
     <div className="flex flex-row gap-3">
-      <div className="flex w-16 h-16 rounded-full bg-dark-grey"></div>
-      <div className="flex flex-col justify-center items-start">
+      <div className="flex w-16 h-16 rounded-full bg-dark-grey">
+        <img
+          src={
+            profile_path
+              ? `https://image.tmdb.org/t/p/original${profile_path}`
+              : defaultProfile
+          }
+          alt="name"
+          className="w-full h-full rounded-full object-cover"
+        />
+      </div>
+      <div className="flex flex-col justify-center items-start overflow-hidden text-ellipsis whitespace-nowrap">
         <h6 className="text-normal font-light font-sarabun text-white">
-          Úrsula Coberó
+          {name}
         </h6>
         <p className="text-normal font-light font-sarabun text-light-grey">
-          as Tokyo
+          as {character}
         </p>
       </div>
     </div>
