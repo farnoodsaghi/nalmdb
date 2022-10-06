a24da873676c7c1bb025b14eb80f74a6fe1a7e08
Farnood Saghi <farnood@Farnoods-MacBook-Air.local>
2022-10-07 05:05:23 +0800

    feat: create a new component for genre tags in single title page

--- client/src/components/GenreTag.tsx
+++ client/src/components/GenreTag.tsx
@@ -6,7 +6,7 @@ interface GenreTagProps {
 
 const GenreTag: React.FC<GenreTagProps> = ({ label }) => {
   return (
-    <span className="flex justify-center items-center bg-darker-grey text-sm font-light font-sarabun text-white rounded-full w-16 h-6">
+    <span className="flex justify-center items-center bg-darker-grey text-sm font-light font-sarabun text-white rounded-full px-3 py-2 h-6">
       {label}
     </span>
   );
