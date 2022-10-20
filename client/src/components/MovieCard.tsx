e69cd49ce6d9c7c38aad61eae9eb5390aabb6a47
Farnood Saghi <farnood@Farnoods-MacBook-Air.local>
2022-10-21 00:12:09 +0800

    feat: add some on click  func for determining if a title is a movie or tv show

--- client/src/components/MovieCard.tsx
+++ client/src/components/MovieCard.tsx
@@ -1,15 +1,31 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { MoviesContext } from "../context/moviesContext";
 
 interface MovieCardProps {
   children?: React.ReactNode;
   poster_path: string;
   id: string;
+  media_type: undefined | string;
+  first_air_date: undefined | string;
 }
 
-const MovieCard: React.FC<MovieCardProps> = ({ poster_path, id }) => {
+const MovieCard: React.FC<MovieCardProps> = ({
+  poster_path,
+  id,
+  media_type,
+  first_air_date,
+}) => {
+  const { handleMediaType } = React.useContext(MoviesContext)!;
   return (
-    <div className="flex justify-center items-center bg-dark-grey rounded-md cursor-pointer hover:brightness-75 snap-start max-h-full aspect-[1/1.5]">
+    <div
+      onClick={() => {
+        handleMediaType(
+          media_type ? media_type : first_air_date ? "tv" : "movie"
+        );
+      }}
+      className="flex justify-center items-center bg-dark-grey rounded-md cursor-pointer hover:brightness-75 snap-start max-h-full aspect-[1/1.5]"
+    >
       <Link to={`/title/${id}`}>
         <img
           src={`https://image.tmdb.org/t/p/original/${poster_path}`}
