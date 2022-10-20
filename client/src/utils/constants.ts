9076494057855f68885e5b8febd65434878b0593
Farnood Saghi <farnood@Farnoods-MacBook-Air.local>
2022-10-21 00:16:33 +0800

    feat: add some constants and helper functions

--- client/src/utils/constants.ts
+++ client/src/utils/constants.ts
@@ -3,7 +3,66 @@ type TopBarObj = {
   name: string;
 };
 
+type SideBarItems = {
+  menu: SideBarMenu[];
+  library: SideBarMenu[];
+};
+
+type SideBarMenu = {
+  id: number;
+  icon: string;
+  name: string;
+};
+
+type Sort = {
+  id: number;
+  name: string;
+};
+
+type Genre = {
+  id: number;
+  name: string;
+};
+
 export const TOP_BAR_ITEMS: TopBarObj[] = [
   { id: 1, name: "Movies" },
   { id: 2, name: "TV Shows" },
+];
+
+export const SIDEBAR_MENU: SideBarMenu[] = [
+  { id: 1, name: "Home", icon: "fluent:home-16-filled" },
+  { id: 2, name: "Browse", icon: "fa-regular:compass" },
+  { id: 3, name: "Top Rated", icon: "ant-design:star-filled" },
+  { id: 4, name: "Trending", icon: "bxs:bar-chart-alt-2" },
+];
+
+export const SIDEBAR_ITEMS: SideBarItems = {
+  menu: [
+    { id: 1, name: "Home", icon: "fluent:home-16-filled" },
+    { id: 2, name: "Browse", icon: "fa-regular:compass" },
+    { id: 3, name: "Top Rated", icon: "ant-design:star-filled" },
+    { id: 4, name: "Trending", icon: "bxs:bar-chart-alt-2" },
+  ],
+  library: [
+    { id: 5, name: "Reviews", icon: "material-symbols:reviews-outline" },
+    { id: 6, name: "Watchlist", icon: "heroicons:bookmark-solid" },
+  ],
+};
+
+export const SORT_VALUES: Sort[] = [
+  { id: 1, name: "Popularity Ascending" },
+  { id: 2, name: "Popularity Descending" },
+  { id: 3, name: "Rating: High to Low" },
+  { id: 4, name: "Rating: Low to High" },
+  { id: 5, name: "Name: A to Z" },
+  { id: 6, name: "Name: Z to A" },
+];
+
+export const GENRE_VALUES: Genre[] = [
+  { id: 1, name: "Action" },
+  { id: 2, name: "Comedy" },
+  { id: 3, name: "Crime" },
+  { id: 4, name: "Drama" },
+  { id: 5, name: "Fantasy" },
+  { id: 6, name: "Scifi" },
 ];
--- client/src/utils/helpers.ts
+++ client/src/utils/helpers.ts
@@ -8,6 +8,11 @@ interface CastEndpoint {
   fetchTVCast: string;
 }
 
+interface Values {
+  id: number;
+  name: string;
+}
+
 export const getSingleTitleEndpoint = (id: string): SingleTitleEndpoint => {
   return {
     fetchSingleMovie: `/movie/${id}?api_key=${process.env.REACT_APP_MOVIE_API_KEY}&language=en-US`,
@@ -37,4 +42,32 @@ export const formatTime = (minutes: number): string => {
   let roundedHours = Math.floor(hours);
   let remainingMinutes = (hours - roundedHours) * 60;
   return `${roundedHours}h ${Math.round(remainingMinutes)}m`;
+};
+
+export const findValById = (values: Values[], id: number): string => {
+  let currentVal = values.find((val) => val.id === id);
+  return currentVal!.name;
+};
+
+export const areObjectsEqual = (obj1: any, obj2: any) => {
+  const keys1 = Object.keys(obj1),
+    keys2 = Object.keys(obj2);
+  const vals1 = Object.values(obj1),
+    vals2 = Object.values(obj2);
+  return (
+    keys1.length === keys2.length &&
+    keys1.every((key) => keys2.includes(key)) &&
+    vals1.length === vals2.length &&
+    vals1.every((val) => vals2.includes(val))
+  );
+};
+
+export const isEqual = (arr: any[], obj: any) => {
+  const found = arr.filter((item: any) => {
+    return (
+      item.id === obj.id &&
+      (item.name || item.title) === (obj.name || obj.title)
+    );
+  });
+  return found.length > 0;
 };
