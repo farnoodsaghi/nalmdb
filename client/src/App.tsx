3868c63b14cd15cde02d08bc73c296b9995d3d46
Farnood Saghi <farnood@Farnoods-MacBook-Air.local>
2022-10-21 00:02:47 +0800

    feat: add routes for items in sidebar

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -7,6 +7,11 @@ import {
   Signup,
   Login,
   SingleTitle,
+  Trending,
+  TopRated,
+  Browse,
+  Reviews,
+  WatchList,
 } from "./pages/index";
 
 interface AppProps {}
@@ -17,6 +22,12 @@ const App: React.FC<AppProps> = ({}) => {
       <Routes>
         <Route path="/" element={<SharedLayout />}>
           <Route index element={<Home />} />
+          <Route path="home" element={<Home />} />
+          <Route path="browse" element={<Browse />} />
+          <Route path="toprated" element={<TopRated />} />
+          <Route path="trending" element={<Trending />} />
+          <Route path="reviews" element={<Reviews />} />
+          <Route path="watchlist" element={<WatchList />} />
         </Route>
         <Route path="/title/:id" element={<SingleTitle />} />
         <Route path="/signup" element={<Signup />} />
