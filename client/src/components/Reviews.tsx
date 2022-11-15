9ce9c7e5eb19de9ff4ff9368d73d669c7242036c
Farnood Saghi <farnood@Farnoods-MacBook-Air.local>
2022-11-16 03:47:35 +0800

    feat: add review list component

--- client/src/components/Reviews.tsx
+++ client/src/components/Reviews.tsx
@@ -2,71 +2,18 @@ import React, { useState } from "react";
 import { Icon } from "@iconify/react";
 import defaultProfile from "../assets/default_profile_path.png";
 
-interface ReviewsProps {}
+interface ReviewsProps {
+  user: string;
+  rating: string;
+  content: string;
+}
 
-const Reviews: React.FC<ReviewsProps> = ({}) => {
+const Reviews: React.FC<ReviewsProps> = ({ user, rating, content }) => {
   const [liked, setLiked] = useState(false);
+  const [stars, setStars] = useState<number[]>(
+    new Array(5).fill(0).map((_, index) => index + 1)
+  );
   return (
-    // <section className="flex flex-col justify-center items-start gap-3">
-    //   <div className="flex flex-row justify-between items-start gap-4">
-    //     <img
-    //       src={defaultProfile}
-    //       alt="profile-pic"
-    //       className="rounded-full w-14 h-14"
-    //     />
-    //     <div className="flex flex-col justify-center items-start gap-1 text-white">
-    //       <h4 className="text-base font-semibold font-sarabun text-white">
-    //         Greg Smith
-    //       </h4>
-    //       <p className="flex flex-row text-sm font-medium font-sarabun text-light-grey gap-2">
-    //         <div className="flex flex-row justify-start items-center gap-1">
-    //           <Icon
-    //             icon="bi:star-fill"
-    //             className="w-5 h-5 text-yellow-400"
-    //           ></Icon>
-    //           <Icon
-    //             icon="bi:star-fill"
-    //             className="w-5 h-5 text-yellow-400"
-    //           ></Icon>
-    //           <Icon
-    //             icon="bi:star-fill"
-    //             className="w-5 h-5 text-yellow-400"
-    //           ></Icon>
-    //           <Icon
-    //             icon="bi:star-fill"
-    //             className="w-5 h-5 text-yellow-400"
-    //           ></Icon>
-    //           <Icon
-    //             icon="bi:star-fill"
-    //             className="w-5 h-5 text-yellow-400"
-    //           ></Icon>
-    //         </div>
-    //         posted 14h ago
-    //       </p>
-    //       <p className="text-base font-normal font-sarabun text-white mt-3">
-    //         "Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti,
-    //         ipsum doloremque at neque ea ducimus? Consectetur doloremque totam,
-    //         nulla, vero accusantium sequi, nesciunt officiis facere aliquam odio
-    //         nobis eum commodi?"
-    //       </p>
-    //     </div>
-    //     <div
-    //       onClick={() => setLiked((prev) => !prev)}
-    //       className="flex flex-row justify-between gap-1 items-center text-light-grey cursor-pointer"
-    //     >
-    //       {liked ? (
-    //         <Icon
-    //           icon="ant-design:heart-filled"
-    //           className={`h-5 w-5 text-rose-700 scale-110`}
-    //         />
-    //       ) : (
-    //         <Icon icon="ant-design:heart-outlined" className={`h-5 w-5`} />
-    //       )}
-    //       <p className="font-sarabun">1.2k</p>
-    //     </div>
-    //   </div>
-    //   <hr className="flex self-center w-full border-t border-dark-grey my-5" />
-    // </section>
     <section className="flex flex-col justify-center items-start gap-3 bg-off-black drop-shadow-md border-b-4 border-royal-purple p-5 mb-8 rounded-t-md">
       <div className="flex flex-row justify-between items-start gap-4">
         <img
@@ -77,36 +24,25 @@ const Reviews: React.FC<ReviewsProps> = ({}) => {
         <div className="flex flex-col justify-center items-start gap-3 text-white">
           <div className="flex flex-row justify-between items-center w-full">
             <div className="flex flex-row justify-start items-center gap-1">
-              <Icon
-                icon="bi:star-fill"
-                className="w-4 h-4 text-yellow-400"
-              ></Icon>
-              <Icon
-                icon="bi:star-fill"
-                className="w-4 h-4 text-yellow-400"
-              ></Icon>
-              <Icon
-                icon="bi:star-fill"
-                className="w-4 h-4 text-yellow-400"
-              ></Icon>
-              <Icon
-                icon="bi:star-fill"
-                className="w-4 h-4 text-yellow-400"
-              ></Icon>
-              <Icon
-                icon="bi:star-fill"
-                className="w-4 h-4 text-yellow-400"
-              ></Icon>
+              {stars.map((id) => {
+                return (
+                  <Icon
+                    icon="bi:star-fill"
+                    className={`w-4 h-4 ${
+                      id <= Number(rating)
+                        ? "text-yellow-400"
+                        : "text-light-grey"
+                    }`}
+                  ></Icon>
+                );
+              })}
             </div>
           </div>
           <p className="text-base font-light font-sarabun text-light-grey w-full">
             <span className="text-base font-light font-sarabun text-cyan-500 mr-2">
-              Greg Smith
+              {user}
             </span>
-            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti,
-            ipsum doloremque at neque ea ducimus? Consectetur doloremque totam,
-            nulla, vero accusantium sequi, nesciunt officiis facere aliquam odio
-            nobis eum commodi?"
+            {content}
           </p>
           <div
             onClick={() => setLiked((prev) => !prev)}
