953a0c70d8bf69e49ccd5677e87cca2521a508e0
Farnood Saghi <farnood@Farnoods-MacBook-Air.local>
2022-12-15 23:49:58 +0800

    feat: add login func to user context and reducer

--- client/src/context/userContext.tsx
+++ client/src/context/userContext.tsx
@@ -1,19 +1,39 @@
 import React, { useReducer, useEffect } from "react";
+import axios from "../axios";
 import { State } from "../reducers/userReducer";
 import reducer from "../reducers/userReducer";
 import { MoviesContext } from "./moviesContext";
 import {
   WATCH_LIST_ADD,
   WATCH_LIST_REMOVE,
-  GET_USER_NAME,
   NEW_REVIEW_SUBMIT,
   USER_REVIEW_LIST_ADD,
+  REVIEW_LIST_REMOVE,
+  TOKEN_REQUEST_START,
+  TOKEN_REQUEST_SUCCESS,
+  TOKEN_REQUEST_ERROR,
+  SESSION_ID_START,
+  SESSION_ID_SUCCESS,
+  SESSION_ID_ERROR,
+  LOGIN,
+  VERIFY_USER,
+  USER_LOGOUT_START,
+  USER_LOGOUT_SUCCESS,
+  USER_LOGOUT_ERROR,
+  ACCOUNT_INFO_START,
+  ACCOUNT_INFO_SUCCESS,
+  ACCOUNT_INFO_ERROR,
 } from "../actions";
+import requests from "../requests";
 
 interface UserContextProps extends State {
   addToWatchlist: (title: Movie) => void;
   removeFromWatchList: (title: Movie) => void;
   handleReviewSubmit: (reviewFormInputs: any) => void;
+  removeFromReviewList: (id: string) => void;
+  fetchTempRequestToken: () => void;
+  createNewSessionId: () => void;
+  handleUserLogout: () => void;
 }
 
 interface UserProviderProps {
@@ -41,10 +61,22 @@ interface Movie {
 const UserContext = React.createContext<UserContextProps | null>(null);
 
 const initialState: State = {
+  is_logged_in: false,
   user_name: "",
+  user_avatar: "",
+  user_loading: false,
+  user_error: false,
   user_watch_list: [],
   current_review: {},
   user_reviews_list: [],
+  request_token_loading: false,
+  request_token: "",
+  request_token_error: false,
+  session_id_loading: false,
+  session_id: "",
+  session_id_error: false,
+  user_logout_loading: false,
+  user_logout_error: false,
 };
 
 const UserProvider: React.FC<UserProviderProps> = ({ children }) => {
@@ -65,15 +97,124 @@ const UserProvider: React.FC<UserProviderProps> = ({ children }) => {
     });
   };
 
-  useEffect(() => {
-    dispatch({ type: GET_USER_NAME, payload: "Greg Smith" });
-  }, []);
+  const removeFromReviewList = (id: string) => {
+    dispatch({ type: REVIEW_LIST_REMOVE, payload: id });
+  };
+
+  const fetchTempRequestToken = async () => {
+    dispatch({ type: TOKEN_REQUEST_START });
+    try {
+      const response = await axios.get(requests.fetchRequestToken);
+      dispatch({
+        type: TOKEN_REQUEST_SUCCESS,
+        payload: response!.data,
+      });
+    } catch (error) {
+      console.log(error);
+      dispatch({ type: TOKEN_REQUEST_ERROR });
+    }
+  };
+
+  const createNewSessionId = async () => {
+    dispatch({ type: SESSION_ID_START });
+    try {
+      const requestToken = localStorage.getItem("request_token");
+      const response = await axios.post(
+        `/authentication/session/new?api_key=${process.env.REACT_APP_MOVIE_API_KEY}`,
+        JSON.stringify({ request_token: requestToken }),
+        {
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+      dispatch({
+        type: SESSION_ID_SUCCESS,
+        payload: response!.data,
+      });
+    } catch (error) {
+      console.log(error);
+      dispatch({ type: SESSION_ID_ERROR });
+    }
+  };
+
+  const verifyUser = () => {
+    const sessionId = localStorage.getItem("session_id");
+    if (sessionId) {
+      dispatch({ type: VERIFY_USER, payload: sessionId });
+    }
+  };
+
+  const handleUserLogout = async () => {
+    dispatch({ type: USER_LOGOUT_START });
+    try {
+      const response = await axios.delete(
+        `/authentication/session?api_key=${process.env.REACT_APP_MOVIE_API_KEY}`,
+        {
+          data: JSON.stringify({ session_id: state.session_id }),
+          headers: { "Content-Type": "application/json" },
+        }
+      );
+      localStorage.removeItem("request_token");
+      localStorage.removeItem("session_id");
+      dispatch({
+        type: USER_LOGOUT_SUCCESS,
+      });
+      window.location.reload();
+    } catch (error) {
+      console.log(error);
+      dispatch({ type: USER_LOGOUT_ERROR });
+    }
+  };
+
+  const fetchAccountDetails = async () => {
+    dispatch({ type: ACCOUNT_INFO_START });
+    try {
+      const response = await axios.get(
+        `${requests.fetchAccountInfo}&session_id=${state.session_id}`
+      );
+      dispatch({
+        type: ACCOUNT_INFO_SUCCESS,
+        payload: response!.data,
+      });
+    } catch (error) {
+      console.log(error);
+      dispatch({ type: ACCOUNT_INFO_ERROR });
+    }
+  };
 
   useEffect(() => {
     if (Object.keys(state.current_review).length > 0) {
       dispatch({ type: USER_REVIEW_LIST_ADD, payload: state.current_review });
     }
   }, [state.current_review]);
+
+  useEffect(() => {
+    if (state.request_token) {
+      localStorage.setItem("request_token", state.request_token);
+      window.location.replace(
+        `https://www.themoviedb.org/authenticate/${state.request_token.toString()}?redirect_to=${
+          process.env.REACT_APP_MAIN_DOMAIN
+        }`
+      );
+    }
+  }, [state.request_token]);
+
+  useEffect(() => {
+    if (state.session_id) {
+      dispatch({ type: LOGIN });
+      localStorage.setItem("session_id", state.session_id);
+      fetchAccountDetails();
+    }
+  }, [state.session_id]);
+
+  useEffect(() => {
+    if (localStorage.getItem("session_id")) {
+      verifyUser();
+    } else if (localStorage.getItem("request_token")) {
+      createNewSessionId();
+    }
+  }, []);
 
   return (
     <UserContext.Provider
@@ -82,6 +223,10 @@ const UserProvider: React.FC<UserProviderProps> = ({ children }) => {
         addToWatchlist,
         removeFromWatchList,
         handleReviewSubmit,
+        removeFromReviewList,
+        fetchTempRequestToken,
+        createNewSessionId,
+        handleUserLogout,
       }}
     >
       {children}
--- client/src/reducers/userReducer.tsx
+++ client/src/reducers/userReducer.tsx
@@ -1,16 +1,42 @@
 import {
   WATCH_LIST_ADD,
   WATCH_LIST_REMOVE,
-  GET_USER_NAME,
   NEW_REVIEW_SUBMIT,
   USER_REVIEW_LIST_ADD,
+  REVIEW_LIST_REMOVE,
+  TOKEN_REQUEST_START,
+  TOKEN_REQUEST_SUCCESS,
+  TOKEN_REQUEST_ERROR,
+  SESSION_ID_START,
+  SESSION_ID_SUCCESS,
+  SESSION_ID_ERROR,
+  LOGIN,
+  VERIFY_USER,
+  USER_LOGOUT_START,
+  USER_LOGOUT_SUCCESS,
+  USER_LOGOUT_ERROR,
+  ACCOUNT_INFO_START,
+  ACCOUNT_INFO_SUCCESS,
+  ACCOUNT_INFO_ERROR,
 } from "../actions";
 
 export interface State {
-  user_name: String;
+  is_logged_in: boolean;
+  user_name: string;
+  user_avatar: string;
+  user_loading: boolean;
+  user_error: boolean;
   user_watch_list: Movie[];
   current_review: any;
   user_reviews_list: any[];
+  request_token_loading: boolean;
+  request_token: string;
+  request_token_error: boolean;
+  session_id_loading: boolean;
+  session_id: string;
+  session_id_error: boolean;
+  user_logout_loading: boolean;
+  user_logout_error: boolean;
 }
 
 interface Action {
@@ -37,9 +63,6 @@ interface Movie {
 }
 
 const reducer = (state: State, action: Action): State => {
-  if (action.type === GET_USER_NAME) {
-    return { ...state, user_name: action.payload };
-  }
   if (action.type === WATCH_LIST_ADD) {
     let newWatchList = [...state.user_watch_list];
     newWatchList.unshift(action.payload);
@@ -63,6 +86,116 @@ const reducer = (state: State, action: Action): State => {
     return {
       ...state,
       user_reviews_list: [...state.user_reviews_list, action.payload],
+    };
+  }
+  if (action.type === REVIEW_LIST_REMOVE) {
+    return {
+      ...state,
+      user_reviews_list: state.user_reviews_list.filter((review) => {
+        return review.review_id !== action.payload;
+      }),
+    };
+  }
+  if (action.type === TOKEN_REQUEST_START) {
+    return {
+      ...state,
+      request_token_loading: true,
+      request_token_error: false,
+    };
+  }
+  if (action.type === TOKEN_REQUEST_SUCCESS) {
+    const { request_token } = action.payload;
+    return {
+      ...state,
+      request_token_loading: false,
+      request_token: request_token,
+    };
+  }
+  if (action.type === TOKEN_REQUEST_ERROR) {
+    return {
+      ...state,
+      request_token_loading: false,
+      request_token_error: true,
+    };
+  }
+  if (action.type === SESSION_ID_START) {
+    return {
+      ...state,
+      session_id_loading: true,
+      session_id_error: false,
+    };
+  }
+  if (action.type === SESSION_ID_SUCCESS) {
+    const { session_id } = action.payload;
+    return {
+      ...state,
+      session_id_loading: false,
+      session_id: session_id,
+    };
+  }
+  if (action.type === SESSION_ID_ERROR) {
+    return {
+      ...state,
+      session_id_loading: false,
+      session_id_error: true,
+    };
+  }
+  if (action.type === LOGIN) {
+    return { ...state, is_logged_in: true };
+  }
+  if (action.type === VERIFY_USER) {
+    return { ...state, session_id: action.payload };
+  }
+  if (action.type === USER_LOGOUT_START) {
+    return {
+      ...state,
+      user_logout_loading: true,
+      user_logout_error: false,
+    };
+  }
+  if (action.type === USER_LOGOUT_SUCCESS) {
+    return {
+      ...state,
+      user_logout_loading: false,
+      session_id: "",
+      request_token: "",
+      is_logged_in: false,
+    };
+  }
+  if (action.type === USER_LOGOUT_ERROR) {
+    return {
+      ...state,
+      user_logout_loading: false,
+      user_logout_error: true,
+    };
+  }
+  if (action.type === ACCOUNT_INFO_START) {
+    return {
+      ...state,
+      user_loading: true,
+      user_error: false,
+    };
+  }
+  if (action.type === ACCOUNT_INFO_SUCCESS) {
+    const {
+      avatar: {
+        gravatar: { hash },
+      },
+      name,
+      username,
+    } = action.payload;
+    return {
+      ...state,
+      user_loading: false,
+      user_name: name || username,
+      user_avatar: `https://www.gravatar.com/avatar/${hash}`,
+    };
+  }
+  if (action.type === ACCOUNT_INFO_ERROR) {
+    return {
+      ...state,
+      user_loading: false,
+      user_error: true,
     };
   }
   throw Error("Invalid action type");
