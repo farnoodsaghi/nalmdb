a8f187f6b9d07745ea723fb066b0f5800b6138d4
Farnood Saghi <farnood@Farnoods-MacBook-Air.local>
2022-12-15 23:44:46 +0800

    feat: add the review page contents

--- client/src/pages/Reviews.tsx
+++ client/src/pages/Reviews.tsx
@@ -1,9 +1,20 @@
 import React from "react";
+import { ReviewPreview } from "../components";
+import { UserContext } from "../context/userContext";
 
 interface ReviewsProps {}
 
 const Reviews: React.FC<ReviewsProps> = ({}) => {
-  return <></>;
+  const { user_reviews_list } = React.useContext(UserContext)!;
+  return (
+    <main className="h-full ml-1/6f w-5/6 mt-24 mb-6 bg-carbon-black">
+      <div className="m-12">
+        {user_reviews_list.map((review, index) => {
+          return <ReviewPreview key={index} {...review} />;
+        })}
+      </div>
+    </main>
+  );
 };
 
 export default Reviews;
