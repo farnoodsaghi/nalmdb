673116db12f71739d7df9aec7e1abe0b600d79b7
Farnood Saghi <farnood@Farnoods-MacBook-Air.local>
2022-12-15 23:46:54 +0800

    feat: add title rating to title card when it's hovered over

--- client/src/components/MovieCard.tsx
+++ client/src/components/MovieCard.tsx
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { MoviesContext } from "../context/moviesContext";
+import { CircleRating } from "../components";
 
 interface MovieCardProps {
   children?: React.ReactNode;
@@ -8,6 +9,7 @@ interface MovieCardProps {
   id: string;
   media_type: undefined | string;
   first_air_date: undefined | string;
+  vote_average: number;
 }
 
 const MovieCard: React.FC<MovieCardProps> = ({
@@ -15,8 +17,10 @@ const MovieCard: React.FC<MovieCardProps> = ({
   id,
   media_type,
   first_air_date,
+  vote_average,
 }) => {
   const { handleMediaType } = React.useContext(MoviesContext)!;
+  const [isHovered, setIsHovered] = useState(false);
   return (
     <div
       onClick={() => {
@@ -24,14 +28,25 @@ const MovieCard: React.FC<MovieCardProps> = ({
           media_type ? media_type : first_air_date ? "tv" : "movie"
         );
       }}
-      className="flex justify-center items-center bg-dark-grey rounded-md cursor-pointer hover:brightness-75 snap-start max-h-full aspect-[1/1.5]"
+      onMouseOver={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
+      className="flex justify-center items-center bg-dark-grey rounded-md cursor-pointer snap-start max-h-full aspect-[1/1.5]"
     >
-      <Link to={`/title/${id}`}>
+      <Link to={`/title/${id}`} className="relative">
         <img
           src={`https://image.tmdb.org/t/p/original/${poster_path}`}
           alt=""
-          className="aspect-[1/1.5] object-cover rounded-md"
+          className={`aspect-[1/1.5] object-cover rounded-md ${
+            isHovered && "brightness-[0.60]"
+          }`}
         />
+        <div
+          className={`${
+            isHovered ? "flex" : "hidden"
+          } justify-center items-center top-0 w-full h-full absolute z-10 scale-50`}
+        >
+          <CircleRating rating={vote_average} />
+        </div>
       </Link>
     </div>
   );
