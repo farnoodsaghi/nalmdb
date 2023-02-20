8af24563c44ddbf189a9271a7654279e78c3f102
Farnood Saghi <farnood@Farnoods-MacBook-Air.local>
2023-02-20 21:49:40 +0800

    feat: add responsiveness

--- client/src/components/MovieGridList.tsx
+++ client/src/components/MovieGridList.tsx
@@ -7,8 +7,8 @@ interface MovieGridListProps {
 
 const MovieGridList: React.FC<MovieGridListProps> = ({ list }) => {
   return (
-    <main className="h-full ml-1/6f w-5/6 mt-24 mb-6 bg-carbon-black">
-      <div className="grid xl:gid-cols-7 lg:grid-cols-6 md:grid-cols-5 sm:grid-cols-4 auto-rows-[minmax(min-content,6rem)] gap-6 mx-12">
+    <main className="h-full md:ml-[5.25rem] md:w-[calc(100%-5.25rem)] lg:ml-1/6f lg:w-5/6 mt-24 mb-6 bg-carbon-black">
+      <div className="grid xl:grid-cols-7 lg:grid-cols-6 md:grid-cols-5 sm:grid-cols-4 auto-rows-[minmax(min-content,6rem)] gap-6 mx-12">
         {list.map((title) => {
           return <MovieCard key={title.id} {...title} />;
         })}
